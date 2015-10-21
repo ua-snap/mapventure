d312e1b2825e5ee13bfe087c46a92c89e63da6bc
Bruce Crevensten <bruce.crevensten@gmail.com>
2015-10-21 15:16:40 -0800

    workaround for broken deps in proj4leaflet

--- Gruntfile.js
+++ Gruntfile.js
@@ -183,7 +183,8 @@ module.exports = function (grunt) {
     wiredep: {
       app: {
         src: ['<%= yeoman.app %>/index.html'],
-        ignorePath:  /\.\.\//
+        ignorePath:  /\.\.\//,
+        exclude: ['bower_components/leaflet/']
       },
       test: {
         devDependencies: true,
