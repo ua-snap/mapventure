2c70652eff722413e87ecbc7081c5a9e278192b3
Bob Torgerson <rltorgerson@alaska.edu>
2015-11-03 12:34:23 -0900

    Changes to index.html and karma.conf.js after reverting the PR #10.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -32,7 +32,7 @@ module.exports = function(config) {
       'bower_components/angular-sanitize/angular-sanitize.js',
       'bower_components/angular-touch/angular-touch.js',
       'bower_components/proj4/dist/proj4.js',
-      'bower_components/leaflet-dist/leaflet.js',
+      'bower_components/leaflet/dist/leaflet-src.js',
       'bower_components/proj4leaflet/src/proj4leaflet.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
