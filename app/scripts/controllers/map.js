7f8c6f25e25f7943bef1a6f5959399cd8c2a9680
Bob Torgerson <rltorgerson@alaska.edu>
2015-11-18 14:47:20 -0900

    Added comments to map.js and removed console.log messages for testing events firing.

--- app/scripts/controllers/map.js
+++ app/scripts/controllers/map.js
@@ -25,10 +25,18 @@ app.controller('MapCtrl', [
         $scope.map = data;
         $scope.crs = BaseMap.getCRS($scope.map.srid);
         $scope.baselayer = BaseMap.getBaseLayer($scope.map.srid, geoserverUrl);
+    
+        // The splash screen should be on until Map is loaded.
         $scope.splashHide = false;
+
+        // This variable must be set to be watched or else the Leaflet event does not update the 
+        // ngHide function properly.
         $scope.$watch('splashHide');
-        console.log($scope.splashHide);
-        $scope.baselayer.on("load", function() { console.log("All tiles loaded."); $scope.splashHide = true; $scope.$apply(); });
+
+        // This checks for the 'load' event from Leaflet which means that the basemap 
+        // has completely loaded.
+        $scope.baselayer.on("load", function() { $scope.splashHide = true; $scope.$apply(); });
+
         $scope.addLayers();
 
         $scope.mapObj = L.map('snapmapapp', {
