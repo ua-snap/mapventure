28d606982e50e2362101fe4e1da645c50d676ff4
Bruce Crevensten <bruce.crevensten@gmail.com>
2016-07-11 15:10:23 -0700

    Split global v. per-map features into different controllers
    This moves the Fire-map-specific code into its own controller and keeps common code in the top-level MapCtrl object.  Further refinement is necessary.
    
    The `basemap` Service is commented out since its functionality is moved into the Fire-map-specific controller, but there are some useful notes in there for the pan-Arctic projection, which I'll properly remove/document in a future commit.

--- app/scripts/controllers/map.js
+++ app/scripts/controllers/map.js
@@ -13,11 +13,405 @@ var app = angular.module('mapventureApp');
 app.controller('MapCtrl', [
   '$scope',
   '$controller',
-  function($scope, $controller) {
-    console.log(app);
-    var mapController = $controller(
+  '$http',
+  '$routeParams',
+  '$timeout',
+  'ngDialog',
+  'Map',
+  'Slug',
+  'moment',
+  function($scope, $controller, $http, $routeParams, $timeout, ngDialog, Map, Slug, moment) {
+
+    var GEOSERVER_URL = Map.geoserverUrl();
+    var GEOSERVER_WMS_URL = Map.geoserverWmsUrl();
+    var GEONODE_URL = Map.geonodeUrl();
+    var GEONODE_API_URL = Map.geonodeApiUrl();
+
+    /*
+    These options must be defined by
+    the Map Instance Controller (i.e. the
+    specific Map controller instantiated
+    for this specific map ID), but are
+    listed here for reference.
+    */
+    $scope.defaultLayers = undefined;
+    $scope.mapOptions = undefined;
+    $scope.crs = undefined;
+    $scope.getBaseLayer = undefined;
+    /*
+    End options to be overridden in MapInstance
+    controllers.
+    */
+
+    // Will contain L.Layer.wms objects, keyed by layer name
+    $scope.layers = {};
+
+    // For the download progress bar
+    $scope.progress = 0;
+    $scope.processID = 0;
+
+    // If the Layer menu is minimized?
+    $scope.minimized = false;
+
+    // Two variables to use to keep the last map center/zoom
+    // so we can restore after an auto-zoom
+    $scope.mapCenter = undefined;
+    $scope.zoomLevel = undefined;
+
+    // Dual maps boolean
+    $scope.dualMaps = false;
+
+    // Sync maps boolean
+    $scope.syncMaps = false;
+
+    // The Proceed button on the splash screen
+    // should be dimmed until Map is loaded.
+    $scope.showMapButtonDisabled = true;
+
+    // Create controller for map-specific functionality
+    var mapInstanceController = $controller(
       'AlaskaWildfiresCtrl',
-      { $scope: $scope });
-    console.log('hello');
+      {$scope: $scope}
+    );
+
+    Map.layers($routeParams.mapId).success(function(data) {
+      $scope.map = data;
+
+      $http.get(GEONODE_API_URL + '/maps/' + $scope.map.id).success(function(data) {
+        var converter = new showdown.Converter();
+        var content = converter.makeHtml(data.abstract);
+        angular.element('#splashOverviewContent').html(content);
+      });
+
+      // Attach class name for custom CSS hooks
+      // for this map.  Class name is a slugified
+      // version of the map's title.
+      //
+      // TODO: isolate this entire thing in a
+      // directive of its own?
+      angular.element('body').addClass(Slug.slugify($scope.map.title));
+
+      // Reversing the layers makes the order
+      // match what we see in GeoNode's map editor.
+      $scope.map.layers.reverse();
+
+      // These need to be separate instances because we listen for events differently on each.
+      var baseLayer = $scope.getBaseLayer()
+      var secondBaseLayer = $scope.getBaseLayer();
+
+      // Move to a per-map service?
+      var mapDefaults = angular.extend({
+          center: [65, -150],
+          zoom: 1,
+          crs: $scope.crs,
+          zoomControl: false,
+          scrollWheelZoom: false
+        }, $scope.mapOptions
+      );
+
+      var firstMapOptions = angular.extend({
+          layers: [
+            baseLayer
+          ]
+        },
+        mapDefaults);
+      $scope.mapObj = L.map('snapmapapp', firstMapOptions);
+
+      var secondMapOptions = angular.extend({
+          layers: [
+            secondBaseLayer
+          ]
+        },
+        mapDefaults);
+      $scope.secondMapObj = L.map('secondmap', secondMapOptions);
+
+      // Attach event handlers per-map.
+      // The onLoad() function is defined in the
+      // instance map and attached to this common
+      // scope.
+      $scope.onLoad($scope.mapObj, $scope.secondMapObj, $scope);
+
+      // This checks for the 'load' event from Leaflet which means that the basemap
+      // has completely loaded.
+      baseLayer.on('load', function() {
+        $scope.showMapButtonDisabled = false;
+        $scope.$apply();
+      });
+
+      $scope.addLayers();
+
+      // Show default layers
+      angular.forEach($scope.defaultLayers, function(layerName) {
+        $scope.showLayer(layerName);
+      });
+
+      $scope.mapObj.addControl($scope.sidebar);
+
+      $scope.sortableOptions = {
+        stop: function() {
+          for (var i = 0; i < $scope.map.layers.length; i++) {
+            $scope.layers[$scope.map.layers[i].name].obj.setZIndex($scope.map.layers.length - i);
+            $scope.layers[$scope.map.layers[i].name].secondObj.setZIndex($scope.map.layers.length - i);
+          }
+        }
+      };
+
+      new L.Control.Zoom({position: 'topright'}).addTo($scope.mapObj);
+      new L.Control.Zoom({position: 'topright'}).addTo($scope.secondMapObj);
+
+    });
+
+    $scope.sidebar = L.control.sidebar('info-sidebar', {
+      position: 'left'
+    });
+
+    $scope.sidebar.on('show', function() {
+      $scope.minimizeMenu();
+    });
+
+    $scope.sidebar.on('hide', function() {
+      $scope.minimizeMenu();
+      $scope.$apply();
+    });
+
+    $scope.showSecondLayer = function(layerName) {
+      $scope.layers[layerName].secondObj.addTo($scope.secondMapObj);
+      $scope.layers[layerName].secondvisible = true;
+    };
+
+    $scope.hideSecondLayer = function(layerName) {
+      $scope.secondMapObj.removeLayer($scope.layers[layerName].secondObj);
+      $scope.layers[layerName].secondvisible = false;
+    };
+
+    $scope.toggleSecondLayer = function(layerName) {
+        if ($scope.secondMapObj.hasLayer($scope.layers[layerName].secondObj) === false) {
+          $scope.showSecondLayer(layerName);
+        } else {
+          $scope.hideSecondLayer(layerName);
+        }
+      };
+
+    $scope.addLayers = function() {
+      angular.forEach($scope.map.layers, function(layer) {
+        // Strip the 'geonode:' prefix, not sure how that's used in
+        // GeoExplorer or MapLoom's versions of things.
+        layer.name = layer.name.replace('geonode:','');
+        $scope.layers[layer.name] = {};
+        $scope.layers[layer.name].obj = L.tileLayer.wms(GEOSERVER_WMS_URL, {
+          continuousWorld: true,
+          layers: layer.name,
+          name: layer.name,
+          transparent: true,
+          format: 'image/png',
+          version: '1.3',
+          visible: false
+        });
+        $scope.layers[layer.name].secondObj = L.tileLayer.wms(GEOSERVER_WMS_URL, {
+          continuousWorld: true,
+          layers: layer.name,
+          name: layer.name,
+          transparent: true,
+          format: 'image/png',
+          version: '1.3',
+          visible: false
+        });
+      });
+      Map.setReady(true);
+    };
+
+    // This variable must be watched to allow for the sidebar
+    // of Leaflet to hide and show the layer menu
+    $scope.$watch('minimized');
+
+    // This variable must be set to be watched or else the
+    // Leaflet event does not update the ngHide function properly.
+    $scope.$watch('showMapButtonDisabled');
+
+    $scope.showLayer = function(layerName) {
+      $scope.layers[layerName].obj.addTo($scope.mapObj);
+      $scope.layers[layerName].visible = true;
+    };
+
+    $scope.hideLayer = function(layerName) {
+      $scope.mapObj.removeLayer($scope.layers[layerName].obj);
+      $scope.layers[layerName].visible = false;
+    };
+
+    $scope.toggleLayer = function(layerName) {
+      if ($scope.mapObj.hasLayer($scope.layers[layerName].obj) === false) {
+        $scope.showLayer(layerName);
+      } else {
+        $scope.hideLayer(layerName);
+      }
+    };
+
+    $scope.downloadMap = function(mapId) {
+      if ($scope.processID === 0) {
+        $http.get(GEONODE_URL + '/maps/' + mapId + '/immeddownload').then(function(response) {
+          $scope.processID = response.data.id;
+          $scope.checkDownload($scope.processID);
+        });
+      } else {
+        $scope.checkDownload($scope.processID);
+      }
+    };
+
+    $scope.checkDownload = function(processID) {
+      ngDialog.open({
+        template: 'mapDownload',
+        scope: $scope
+      });
+
+      if ($scope.progress <= 0) {
+        $scope.progress = 0;
+        var checkStatus = setInterval(function() {
+          $.ajax({
+            type: 'GET',
+            url: GEOSERVER_URL + '/rest/process/batchDownload/status/' + processID
+          })
+          .done(function(result) {
+            $scope.progress = result.process.progress.toFixed(2);
+            if (result.process.status === 'FINISHED') {
+              window.open(GEOSERVER_URL + '/rest/process/batchDownload/download/' +  processID, '_blank');
+              $scope.progress = -1;
+              $scope.processID = 0;
+              clearInterval(checkStatus);
+            } else if (result.process.status === 'ERROR') {
+              $scope.progress = -2;
+              $scope.processID = 0;
+              clearInterval(checkStatus);
+            }
+            $scope.$apply();
+          });
+        }, 1000);
+      }
+    };
+
+    $scope.$on('show-layers', function(event, showLayers) {
+      angular.forEach($scope.layers, function(value, layerName) {
+        if (showLayers.indexOf(layerName) !== -1) {
+          $scope.$evalAsync(function() {
+            $scope.showLayer(layerName);
+          });
+        } else {
+          $scope.$evalAsync(function() {
+            $scope.hideLayer(layerName);
+          });
+        }
+      });
+    });
+
+    $scope.minimizeMenu = function() {
+      if ($scope.minimized === false) {
+        $scope.minimized = true;
+      } else {
+        $scope.minimized = false;
+      }
+    };
+
+    $scope.$on('show-second-layers', function(event, showLayers) {
+      angular.forEach($scope.layers, function(value, layerName) {
+        if (showLayers.indexOf(layerName) !== -1) {
+          $scope.$evalAsync(function() {
+            $scope.showSecondLayer(layerName);
+          });
+        } else {
+          $scope.$evalAsync(function() {
+            $scope.hideSecondLayer(layerName);
+          });
+        }
+      });
+    });
+
+    $scope.$on('start-tour-dual-maps', function() {
+      $scope.$evalAsync(function() {
+        if ($scope.dualMaps === true) {
+          $scope.showDualMaps();
+        }
+      });
+    });
+
+    $scope.$on('show-dual-maps', function() {
+      $scope.$evalAsync(function() {
+        $scope.showDualMaps();
+      });
+    });
+
+    $scope.$on('show-sync-maps', function() {
+      $scope.$evalAsync(function() {
+        $scope.syncDualMaps();
+      });
+    });
+
+    $scope.showMapInformation = function(mapId) {
+      $http.get(GEONODE_API_URL + '/maps/' + mapId).success(function(data) {
+        var converter = new showdown.Converter();
+        var content = '<p><a href="' + data.urlsuffix + '">' + data.urlsuffix + '</a></p>';
+        content = content.concat(converter.makeHtml(data.abstract));
+        $scope.sidebar.setContent(content).show();
+      });
+    };
+
+    $scope.showDualMaps = function() {
+      if ($scope.dualMaps === false) {
+        $scope.dualMaps = true;
+        $timeout(function() {
+          $scope.mapObj.invalidateSize();
+          $scope.mapObj.panTo([65, -150]);
+          $scope.secondMapObj.panTo([65, -150]);
+        }, 250);
+      } else {
+        $scope.dualMaps = false;
+        if ($scope.syncMaps === true) {
+          $scope.syncDualMaps();
+        }
+        $timeout(function() {
+          $scope.mapObj.invalidateSize();
+          $scope.mapObj.panTo([65, -150]);
+        }, 250);
+      }
+    };
+
+    $scope.syncDualMaps = function() {
+      if ($scope.syncMaps === false) {
+        $scope.syncMaps = true;
+        $scope.mapObj.sync($scope.secondMapObj);
+        $scope.secondMapObj.sync($scope.mapObj);
+      } else {
+        $scope.syncMaps = false;
+        $scope.mapObj.unsync($scope.secondMapObj);
+        $scope.secondMapObj.unsync($scope.mapObj);
+      }
+    };
+
+    $scope.showLayerInformation = function(layerName) {
+      var layer = _.find($scope.map.layers, function(layer) {
+        return layer.name === layerName;
+      });
+      var converter = new showdown.Converter();
+      var content = '<h3>' + layer.capability.title + '</h3>';
+      content = content.concat(
+        '<img id="legend" src="' +
+        GEOSERVER_WMS_URL +
+        '?REQUEST=GetLegendGraphic&VERSION=1.0.0&FORMAT=image/png&WIDTH=20&HEIGHT=20&LAYER=' +
+        layerName +
+        '" alt="legend" />'
+      );
+      content = content.concat(converter.makeHtml(layer.capability.abstract));
+      $scope.sidebar.setContent(content).show();
+    };
+
+    $scope.startTour = function() {
+      /* These map IDs will need to be changed based
+         upon the map ID of the system running the GeoNode
+         instance. */
+      if ($routeParams.mapId === 2) {
+        $scope.$emit('iem-start-tour');
+      } else if ($routeParams.mapId === 8) {
+        $scope.$emit('ncep-start-tour');
+      }
+    };
+
   }
 ]);
--- app/scripts/services/basemap.js
+++ app/scripts/services/basemap.js
@@ -1,161 +1,139 @@
-'use strict';
+// 'use strict';
 
-/**
- * @ngdoc service
- * @name mapventureApp.BaseMap
- * @description
- * # BaseMap
- * Factory in the mapventureApp.
- */
-angular.module('mapventureApp')
-  .factory('BaseMap', ['$http', 'Map',  function($http, Map) {
+// /**
+//  * @ngdoc service
+//  * @name mapventureApp.BaseMap
+//  * @description
+//  * # BaseMap
+//  * Factory in the mapventureApp.
+//  */
+// angular.module('mapventureApp')
+//   .factory('BaseMap', ['$http', 'Map',  function($http, Map) {
 
-    var service = {};
-    var GEOSERVER_URL = Map.geoserverUrl();
+//     var service = {};
+//     var GEOSERVER_URL = Map.geoserverUrl();
 
-    // Returns an array of layers that should be toggled
-    // to a visible state upon map load.
-    // TODO: refactor to use slug/UUID
-    service.getDefaultLayers = function(mapId) {
-      var defaultLayers = ['active_fires'];
-      return (5 === mapId) ? defaultLayers : [];
-    };
+//     // Returns an array of layers that should be toggled
+//     // to a visible state upon map load.
+//     // TODO: refactor to use slug/UUID
+//     service.getDefaultLayers = function(mapId) {
+//       var defaultLayers = ['active_fires'];
+//       return (5 === mapId) ? defaultLayers : [];
+//     };
 
-    // This is a hook for running scripts when the map is first loaded.
-    service.onLoad = function(mapObj, secondMapObj, $scope) {
-      // For refactoring, this would invoke
-      // a method in a "subclass"; for this first map,
-      // we hardcode -- fire a request to get all
-      // polygons!
+//     // Attach additional per-map handlers.
+//     // mapObj, secondMapObj are both Leaflet map objects
+//     // TODO: pretty sure we don't want to inject scope here.
+//     // Needs refactor.
+//     // Like the 'onLoad' code, this would be called per-map.
+//     // service.attachEventHandlers = function(mapObj, secondMapObj, $scope) {};
 
-      var baseUrl = GEOSERVER_URL + '/wfs?service=wfs&version=2.0.0&request=GetFeature&typeName=geonode:active_fires&srsName=EPSG:3338&outputFormat=application/json&bbox=';
-      var requestUrl = baseUrl +
-        '-2255938.4795,' +
-        '449981.1884,' +
-        '1646517.6368,' +
-        '2676986.5642';
-      $http.get(requestUrl).then(function success(res) {
-        $scope.fireInfoPopup = res;
-      },
-      function error() {
-        $scope.fireInfoPopup = false;
-      });
+//     /**
+//       Get CRS Function
+//       Purpose: Used to generate the desired coordinate reference system for a particular map
+//       Input: epsgCode - EPSG Code as integer value.
+//       Output: Returns a new Leaflet CRS object using the correct options for the input EPSG code
+//     */
+//     service.getCRS = function(epsgCode) {
+//       if (epsgCode === 'EPSG:3572') {
+//         var proj = new L.Proj.CRS('EPSG:3572',
+//             '+proj=laea +lat_0=90 +lon_0=-150 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs',
+//               {
+//                 resolutions: [8192, 4096, 2048, 1024, 512, 256, 128],
+//                 origin: [0, 0]
+//               }
+//           );
 
-    };
+//         // trust me.
+//         // Without this (= pi/2), proj4js returns an undefined
+//         // value for tiles requested at the North Pole and
+//         // it causes a runtime exception.
+//         proj.projection._proj.oProj.phi0 = 1.5708;
+//         return proj;
 
-    // Attach additional per-map handlers.
-    // mapObj, secondMapObj are both Leaflet map objects
-    // TODO: pretty sure we don't want to inject scope here.
-    // Needs refactor.
-    // Like the 'onLoad' code, this would be called per-map.
-    // service.attachEventHandlers = function(mapObj, secondMapObj, $scope) {};
+//       } else {
+//         return new L.Proj.CRS('EPSG:3338',
+//             '+proj=aea +lat_1=55 +lat_2=65 +lat_0=50 +lon_0=-154 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs',
+//               {
+//                 resolutions: [65536, 32768, 16384, 8192, 4096, 2048, 1024, 512, 256, 128, 64, 32, 16],
+//                 origin: [0, 0]
+//               }
+//           );
+//       }
+//     };
 
-    /**
-      Get CRS Function
-      Purpose: Used to generate the desired coordinate reference system for a particular map
-      Input: epsgCode - EPSG Code as integer value.
-      Output: Returns a new Leaflet CRS object using the correct options for the input EPSG code
-    */
-    service.getCRS = function(epsgCode) {
-      if (epsgCode === 'EPSG:3572') {
-        var proj = new L.Proj.CRS('EPSG:3572',
-            '+proj=laea +lat_0=90 +lon_0=-150 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs',
-              {
-                resolutions: [8192, 4096, 2048, 1024, 512, 256, 128],
-                origin: [0, 0]
-              }
-          );
+//     // Returns additional options per map ID.
+//     // These override base map options specified
+//     // in controllers/map.js
+//     // This is hardcoded per map ID currently;
+//     // future revisions could use a slugified
+//     // version of the map title to return
+//     // per-map configurations.
+//     service.getMapOptions = function(mapId) {
+//       var mapOptions = {
+//         zoom: 3,
+//         minZoom: 6,
+//         maxZoom: 11,
+//         maxBounds: new L.latLngBounds(
+//           L.latLng(70.5, -175),
+//           L.latLng(50, -135)
+//         )
+//       };
 
-        // trust me.
-        // Without this (= pi/2), proj4js returns an undefined
-        // value for tiles requested at the North Pole and
-        // it causes a runtime exception.
-        proj.projection._proj.oProj.phi0 = 1.5708;
-        return proj;
+//       // TODO: replace with slug or UUID
+//       return (5 === mapId) ? mapOptions : {};
+//     };
 
-      } else {
-        return new L.Proj.CRS('EPSG:3338',
-            '+proj=aea +lat_1=55 +lat_2=65 +lat_0=50 +lon_0=-154 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs',
-              {
-                resolutions: [65536, 32768, 16384, 8192, 4096, 2048, 1024, 512, 256, 128, 64, 32, 16],
-                origin: [0, 0]
-              }
-          );
-      }
-    };
+//     /**
+//       Get Base Layer Function
+//       Purpose: Generates the base layer from a WMS layer provided by the input to the function.
+//       Definition: Base layers are non-toggleable layers used to place other layers on top of.
+//       Input: epsgCode - EPSG Code as integer value.
+//              layerUrl - Full URL to web-accessible WMS service providing our imported baselayer.
+//              maximumZoom - The maximum number of resolutions available from the created CRS.
+//       Output: Returns a new Leaflet WMS layer object created from the layerURL input.
+//     */
+//     service.getBaseLayer = function(epsgCode, layerUrl) {
 
-    // Returns additional options per map ID.
-    // These override base map options specified
-    // in controllers/map.js
-    // This is hardcoded per map ID currently;
-    // future revisions could use a slugified
-    // version of the map title to return
-    // per-map configurations.
-    service.getMapOptions = function(mapId) {
-      var mapOptions = {
-        zoom: 3,
-        minZoom: 6,
-        maxZoom: 11,
-        maxBounds: new L.latLngBounds(
-          L.latLng(70.5, -175),
-          L.latLng(50, -135)
-        )
-      };
+//       // Real cheap for the moment.
+//       // The `epsgCode` comes from the `SRS` field of the map object
+//       // we get back from the MapLayers API endpoint.  It defaults
+//       // to 4326, and you need to use the Django admin to change it,
+//       // this is just a safety catch.
+//       if (epsgCode === 'EPSG:4326') { epsgCode = 'EPSG:3338'; }
 
-      // TODO: replace with slug or UUID
-      return (5 === mapId) ? mapOptions : {};
-    };
+//       // When we add a 3rd map, we'll need to figure
+//       // out where to refactor this logic.
+//       // These options are merged into the options array
+//       // that Leaflet uses to configure the WMS layer,
+//       // so the names of keys matter.
+//       var layerConfiguration = {
+//         'EPSG:3572': {
+//           layers: 'ne_10m_coastline',
+//           zIndex: 10000,
+//           transparent: true
+//         },
+//         'EPSG:3338': {
+//           layers: 'MapProxy:osm',
+//           transparent: true,
+//           minZoom: 6,
+//           maxZoom: 11
+//         }
+//       };
 
-    /**
-      Get Base Layer Function
-      Purpose: Generates the base layer from a WMS layer provided by the input to the function.
-      Definition: Base layers are non-toggleable layers used to place other layers on top of.
-      Input: epsgCode - EPSG Code as integer value.
-             layerUrl - Full URL to web-accessible WMS service providing our imported baselayer.
-             maximumZoom - The maximum number of resolutions available from the created CRS.
-      Output: Returns a new Leaflet WMS layer object created from the layerURL input.
-    */
-    service.getBaseLayer = function(epsgCode, layerUrl) {
+//       var baseConfiguration = {
+//         format: 'image/png',
+//         version: '1.3',
+//         minZoom: 6,
+//         maxZoom: 11,
+//         continuousWorld: true, // needed for non-3857 projs
+//         noWrap: true, // may be needed for non-3857 projs
+//         zIndex: null
+//       };
 
-      // Real cheap for the moment.
-      // The `epsgCode` comes from the `SRS` field of the map object
-      // we get back from the MapLayers API endpoint.  It defaults
-      // to 4326, and you need to use the Django admin to change it,
-      // this is just a safety catch.
-      if (epsgCode === 'EPSG:4326') { epsgCode = 'EPSG:3338'; }
+//       angular.extend(baseConfiguration, layerConfiguration[epsgCode]);
 
-      // When we add a 3rd map, we'll need to figure
-      // out where to refactor this logic.
-      // These options are merged into the options array
-      // that Leaflet uses to configure the WMS layer,
-      // so the names of keys matter.
-      var layerConfiguration = {
-        'EPSG:3572': {
-          layers: 'ne_10m_coastline',
-          zIndex: 10000,
-          transparent: true
-        },
-        'EPSG:3338': {
-          layers: 'MapProxy:osm',
-          transparent: true,
-          minZoom: 6,
-          maxZoom: 11
-        }
-      };
-
-      var baseConfiguration = {
-        format: 'image/png',
-        version: '1.3',
-        minZoom: 6,
-        maxZoom: 11,
-        continuousWorld: true, // needed for non-3857 projs
-        noWrap: true, // may be needed for non-3857 projs
-        zIndex: null
-      };
-
-      angular.extend(baseConfiguration, layerConfiguration[epsgCode]);
-
-      return new L.tileLayer.wms(layerUrl, baseConfiguration);
-    };
-    return service;
-  }]);
+//       return new L.tileLayer.wms(layerUrl, baseConfiguration);
+//     };
+//     return service;
+//   }]);
