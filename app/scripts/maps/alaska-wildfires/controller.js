5a6785d9c4fac7eb4f79dd0881062dc2b23cd3e8
Bob Torgerson <rltorgerson@alaska.edu>
2016-08-12 14:34:23 -0800

    Auto included semi-colon to end of function.

--- app/scripts/maps/alaska-wildfires/controller.js
+++ app/scripts/maps/alaska-wildfires/controller.js
@@ -110,7 +110,7 @@ app.controller('AlaskaWildfiresCtrl', [
             // Active fire polygons are red-ish,
             // inactive are grey.
             style: function(feature) {
-              if(feature.properties.ACTIVENOW == 1) {
+              if (feature.properties.ACTIVENOW == 1) {
                 return {
                   color: '#ff0000',
                   fillColor: '#E83C18',
@@ -203,22 +203,22 @@ of their polygon.
 http://stackoverflow.com/questions/22796520/finding-the-center-of-leaflet-polygon
 
 */
-var getCentroid2 = function (arr) {
-    var twoTimesSignedArea = 0;
-    var cxTimes6SignedArea = 0;
-    var cyTimes6SignedArea = 0;
+var getCentroid2 = function(arr) {
+  var twoTimesSignedArea = 0;
+  var cxTimes6SignedArea = 0;
+  var cyTimes6SignedArea = 0;
 
-    var length = arr.length
+  var length = arr.length;
 
-    var x = function (i) { return arr[i % length][0] };
-    var y = function (i) { return arr[i % length][1] };
+  var x = function(i) { return arr[i % length][0]; };
+  var y = function(i) { return arr[i % length][1]; };
 
-    for ( var i = 0; i < arr.length; i++) {
-        var twoSA = x(i)*y(i+1) - x(i+1)*y(i);
-        twoTimesSignedArea += twoSA;
-        cxTimes6SignedArea += (x(i) + x(i+1)) * twoSA;
-        cyTimes6SignedArea += (y(i) + y(i+1)) * twoSA;
-    }
-    var sixSignedArea = 3 * twoTimesSignedArea;
-    return [ cxTimes6SignedArea / sixSignedArea, cyTimes6SignedArea / sixSignedArea];
-}
+  for (var i = 0; i < arr.length; i++) {
+    var twoSA = x(i) * y(i + 1) - x(i + 1) * y(i);
+    twoTimesSignedArea += twoSA;
+    cxTimes6SignedArea += (x(i) + x(i + 1)) * twoSA;
+    cyTimes6SignedArea += (y(i) + y(i + 1)) * twoSA;
+  }
+  var sixSignedArea = 3 * twoTimesSignedArea;
+  return [cxTimes6SignedArea / sixSignedArea, cyTimes6SignedArea / sixSignedArea];
+};
