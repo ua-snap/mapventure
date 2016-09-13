7bf3dbf31185f8af84e40f40d54a1c3cc7af5c63
Bruce Crevensten <bruce.crevensten@gmail.com>
2016-09-13 14:14:41 -0700

    remove unnecessary `$scope.watch()` calls

--- app/scripts/controllers/map.js
+++ app/scripts/controllers/map.js
@@ -242,14 +242,6 @@ app.controller('MapCtrl', [
       Map.setReady(true);
     };
 
-    // This variable must be watched to allow for the sidebar
-    // of Leaflet to hide and show the layer menu
-    $scope.$watch('minimized');
-
-    // This variable must be set to be watched or else the
-    // Leaflet event does not update the ngHide function properly.
-    $scope.$watch('showMapButtonDisabled');
-
     $scope.showLayer = function(layerName) {
       $scope.layers[layerName].obj.addTo($scope.mapObj);
       $scope.layers[layerName].visible = true;
